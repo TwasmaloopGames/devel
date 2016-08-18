ec6e4b2b6ef2e8eb6649b54548b03df5341dbbe6
TwasmaloopGames <twasmaloopgames@outlook.com>
2016-08-19 00:08:34 +1000

    Add files via upload

---
+++ src/TCypher/mj_algorithm.c
@@ -0,0 +1,26 @@
+#include "mj_algorithm.h"
+
+float sum(int a, int b)
+{
+    return a+b;
+}
+
+float subtract(int a, int b)
+{
+    return a-b;
+}
+
+float multiply(int a, int b)
+{
+    return a*b;
+}
+
+float remainder(int a, int b)
+{
+    return a/b;
+}
+
+float remainder(int a, int b)
+{
+    return a%b;
+}
---
+++ src/TCypher/mj_algorithm.h
@@ -0,0 +1,11 @@
+#ifndef MJ_ALGORITHM_H_INCLUDED
+#define MJ_ALGORITHM_H_INCLUDED
+
+/*  This is some basic math functions.    */
+float sum(int a, int b);
+float subtract(int a, int b);
+float multiply(int a, int b);
+float divide(int a, int b);
+float remainder(int a, int b);
+
+#endif // MJ_ALGORITHM_H_INCLUDED
